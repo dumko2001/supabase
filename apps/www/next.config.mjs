d02b17c01f1df4c54b5e535b5e4d3fe5fa83aadc
Etienne Stalmans <estalmans@gmail.com>
2026-03-07 10:06:48 +0100

    chore: add ph domains

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -43,6 +43,7 @@ const scriptSrc = [
 
 const cspString = [
   `script-src ${scriptSrc}`,
+  "script-src-elem https://ph.supabase.green https://ph.supabase.com",
   "object-src 'none'",
   "base-uri 'none'",
 ].join("; ")
